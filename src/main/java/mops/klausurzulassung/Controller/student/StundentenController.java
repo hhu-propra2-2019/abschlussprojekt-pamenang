59b4d4210805f99a0ea2b12d181d3628ebf4fb68
Your Name <f-mersch@web.de>
2020-03-03 12:17:45 +0100

    Eingabe Feld für token hinzugefügt

--- src/main/java/mops/klausurzulassung/Controller/student/StundentenController.java
+++ src/main/java/mops/klausurzulassung/Controller/student/StundentenController.java
@@ -4,11 +4,17 @@ package mops.klausurzulassung.Controller.student;
         import org.springframework.stereotype.Controller;
         import org.springframework.ui.Model;
         import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StundentenController {
     @GetMapping("/student")
     public String index(Model model) {
+        return "student";
+    }
+    @PostMapping("/student")
+    public String empfangeDaten(Model model,String name, String token){
+    System.out.println(name + " " + token);
         return "student";
     }
 }
