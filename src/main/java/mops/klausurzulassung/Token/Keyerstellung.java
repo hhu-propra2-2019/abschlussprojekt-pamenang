6a6efa11ec4160aa3b51ec1e482714a1dd8bdc4e
floradzi <florian.radziej@gmail.com>
2020-03-04 09:55:17 +0100

    Erstellen der Klasse PDFerstellung

--- src/main/java/mops/klausurzulassung/Token/Keyerstellung.java
+++ src/main/java/mops/klausurzulassung/Token/Keyerstellung.java
@@ -2,8 +2,15 @@ package mops.klausurzulassung.Token;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.cert.CertificateException;
 
 public class Keyerstellung {
 
@@ -19,5 +26,14 @@ public class Keyerstellung {
 
         //Generieren des Keys
         return keyGenerator.generateKey();
+    }
+
+    private void erstelleKeyStore() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        char[] keyStorePassword = "123abc".toCharArray();
+        try(InputStream keyStoreData = new FileInputStream("keystore.ks")){
+            keyStore.load(keyStoreData, keyStorePassword);
+        }
+
     }
 }
---
+++ src/main/java/mops/klausurzulassung/Token/PDFerstellung.java
@@ -0,0 +1,4 @@
+package mops.klausurzulassung.Token;
+
+public class PDFerstellung {
+}
