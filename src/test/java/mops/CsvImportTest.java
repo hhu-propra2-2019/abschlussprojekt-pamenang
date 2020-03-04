a286d3d652cf78e0b0e8343b656459b58b911143
Boroxx <tenelsen.boris@web.de>
2020-03-04 10:38:11 +0100

    csvImportTests implementiert

--- src/test/java/mops/CsvImportTest.java
+++ src/test/java/mops/CsvImportTest.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
@@ -18,6 +23,8 @@ public class CsvImportTest {
     @Autowired
     private MockMvc mockMvc;
 
+
+
     @Test
     public void shouldReturnCsvImportTemplate() throws Exception {
 
@@ -25,4 +32,15 @@ public class CsvImportTest {
         .andExpect(status().isOk());
 
     }
+
+    @Test
+    public void shouldUploadFileIntoContext() throws Exception {
+    MockMultipartFile mockMultipartFile =
+        new MockMultipartFile("datei", "data.csv", "text/csv", "Boris,Tenelsen,4252152,ja\n".getBytes());
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/csvimport").file(mockMultipartFile)).andExpect(status().is3xxRedirection());
+
+
+    }
+
+
 }
