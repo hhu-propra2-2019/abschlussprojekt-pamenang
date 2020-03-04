27cd48e7543fc6ea1d805d11f69ad04893e46055
Zumoari <joshua.kokol@web.de>
2020-03-04 09:38:58 +0100

    Methode erstelleKey erstellen

--- src/main/java/mops/klausurzulassung/Token/Keyerstellung.java
+++ src/main/java/mops/klausurzulassung/Token/Keyerstellung.java
@@ -1,4 +1,23 @@
 package mops.klausurzulassung.Token;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class Keyerstellung {
+
+    KeyGenerator keyGenerator;
+
+    private SecretKey erstelleKey() throws NoSuchAlgorithmException {
+        keyGenerator = KeyGenerator.getInstance("AES");
+
+        //Initialisieren des KeyGenerators
+        SecureRandom secureRandom = new SecureRandom();
+        int keyBitSize = 256;
+        keyGenerator.init(keyBitSize, secureRandom);
+
+        //Generieren des Keys
+        return keyGenerator.generateKey();
+    }
 }
