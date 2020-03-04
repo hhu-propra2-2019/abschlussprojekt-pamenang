725f192ce62c5d21ac5a083d5179afb25fa423e9
Zumoari <joshua.kokol@web.de>
2020-03-04 12:17:56 +0100

    Aendern der Parameter in verifikation

--- src/main/java/mops/klausurzulassung/Token/Tokenverifikation.java
+++ src/main/java/mops/klausurzulassung/Token/Tokenverifikation.java
@@ -2,7 +2,7 @@ package mops.klausurzulassung.Token;
 
 public class Tokenverifikation {
 
-    public static boolean verifikation(String quittung){
+    public static boolean verifikation(String matr, String fachID, String token){
         return true;
     }
 }
