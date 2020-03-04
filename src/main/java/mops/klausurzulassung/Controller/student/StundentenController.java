43f28d50652493034e8fbc375801b7e8dd95b254
Fabian <f-mersch@web.de>
2020-03-04 10:06:49 +0100

    Einrückung verbessert

--- src/main/java/mops/klausurzulassung/Controller/student/StundentenController.java
+++ src/main/java/mops/klausurzulassung/Controller/student/StundentenController.java
@@ -1,24 +1,23 @@
 package mops.klausurzulassung.Controller.student;
 
-
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StundentenController {
-    @GetMapping("/student")
-    public String studentansicht(Model model) {
-        model.addAttribute("meldung", false);
-        return "student";
-    }
+  @GetMapping("/student")
+  public String studentansicht(Model model) {
+    model.addAttribute("meldung", false);
+    return "student";
+  }
 
-    @PostMapping("/student")
-    public String empfangeDaten(Model model,String matrikelnummer, String token, String fach){
-        boolean value = true;
-        model.addAttribute("success", value);
-        model.addAttribute("meldung", true);
-        return "student";
-    }
+  @PostMapping("/student")
+  public String empfangeDaten(Model model, String matrikelnummer, String token, String fach) {
+    boolean value = true;
+    model.addAttribute("success", value);
+    model.addAttribute("meldung", true);
+    return "student";
+  }
 }
