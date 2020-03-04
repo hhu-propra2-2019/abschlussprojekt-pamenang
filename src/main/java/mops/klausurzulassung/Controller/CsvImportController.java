a7a78a0af544170329655a654f130c6316877b15
Boroxx <tenelsen.boris@web.de>
2020-03-04 08:59:43 +0100

    Controller, Template Inhalt hinzugefuegt

--- src/main/java/mops/klausurzulassung/Controller/CsvImportController.java
+++ src/main/java/mops/klausurzulassung/Controller/CsvImportController.java
@@ -1,4 +1,16 @@
 package mops.klausurzulassung.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CsvImportController {
+
+    @GetMapping("/csvimport")
+    public String csvimport(Model model){
+
+        return "csvImport";
+    }
+
 }
