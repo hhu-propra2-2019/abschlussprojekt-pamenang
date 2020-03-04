646305aeb100bedb761b36c0b83b8fa451079071
floradzi <florian.radziej@gmail.com>
2020-03-04 10:28:00 +0100

    Initalisieren der PDFerstellen Methode

--- src/main/java/mops/klausurzulassung/PDF/PDFerstellung.java
+++ src/main/java/mops/klausurzulassung/PDF/PDFerstellung.java
@@ -1,4 +1,30 @@
 package mops.klausurzulassung.PDF;
 
+import java.io.OutputStream;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+
 public class PDFerstellung {
+
+    public void pdfschreiben(OutputStream outputStream) throws DocumentException {
+        //Initalisieren der PDF
+        Document document = new Document();
+        PdfWriter.getInstance(document, outputStream);
+
+        document.open();
+
+        //Gestaltung der PDF
+
+        document.addTitle("KEEEY");
+        document.addSubject("");
+        document.addAuthor("Joshi");
+        document.addKeywords("");
+        document.addCreator("");
+
+
+        document.close();
+
+
+    }
 }
