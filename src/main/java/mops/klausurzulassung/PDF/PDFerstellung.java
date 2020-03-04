3e5f0c5efd339f02652984d1a3029b90bf9b8c56
Zumoari <joshua.kokol@web.de>
2020-03-04 10:53:14 +0100

    Hinzufügen der pdfschreiben Logik

--- src/main/java/mops/klausurzulassung/PDF/PDFerstellung.java
+++ src/main/java/mops/klausurzulassung/PDF/PDFerstellung.java
@@ -1,13 +1,17 @@
 package mops.klausurzulassung.PDF;
 
 import java.io.OutputStream;
+
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class PDFerstellung {
 
-    public void pdfschreiben(OutputStream outputStream) throws DocumentException {
+    public static void pdfschreiben(OutputStream outputStream, String quittung, String matr, String fach) throws DocumentException {
+
         //Initalisieren der PDF
         Document document = new Document();
         PdfWriter.getInstance(document, outputStream);
@@ -16,12 +20,20 @@ public class PDFerstellung {
 
         //Gestaltung der PDF
 
-        document.addTitle("KEEEY");
-        document.addSubject("");
-        document.addAuthor("Joshi");
-        document.addKeywords("");
-        document.addCreator("");
+        document.addTitle("Quittung");
+        document.addSubject("Quittung");
+        document.addAuthor("Uni-Duesseldorf");
 
+        Paragraph paragraph = new Paragraph();
+        paragraph.add(new Chunk("Matrikelnummer: " + matr + "\n" + "Modul: " + fach));
+        document.add(paragraph);
+
+        //Hinzufügen des QR-Codes
+        // -> pending
+
+        paragraph = new Paragraph();
+        paragraph.add(quittung + "\n");
+        document.add(paragraph);
 
         document.close();
 
