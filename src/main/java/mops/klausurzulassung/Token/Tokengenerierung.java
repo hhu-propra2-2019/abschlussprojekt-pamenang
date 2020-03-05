89e38fbd165a4a30477d8d1701fa94e8f6381ae7
floradzi <florian.radziej@gmail.com>
2020-03-05 09:17:54 +0100

    Keygenerierung/Signatur

--- src/main/java/mops/klausurzulassung/Token/Tokengenerierung.java
+++ src/main/java/mops/klausurzulassung/Token/Tokengenerierung.java
@@ -5,7 +5,14 @@ import com.google.common.hash.HashCode;
 import com.google.common.hash.Hasher;
 import com.google.common.hash.Hashing;
 
+import java.security.*;
+
 public class Tokengenerierung {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+
+        signatur();
+    }
 
     public static String hashing(String HashValue){
 
@@ -22,5 +29,39 @@ public class Tokengenerierung {
 
     public static String erstellenQuittung(String matr, String fach, String token){
         return matr+fach+token;
+    }
+
+    public static String signatur() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+
+        //Creating KeyPair generator object
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+
+        //Initializing the key pair generator
+        keyPairGen.initialize(2048);
+
+        //Generate the pair of keys
+        KeyPair pair = keyPairGen.generateKeyPair();
+
+        //Getting the privatekey from the key pair
+        PrivateKey privKey = pair.getPrivate();
+
+        //Creating a Signature object
+        Signature sign = Signature.getInstance("SHA256withRSA");
+
+        //Initializing the signature
+        sign.initSign(privKey);
+        byte[] bytes = "Hello how are you".getBytes();
+
+        //Adding data to the signature
+        sign.update(bytes);
+
+        //Calculating the signature
+        byte[] signature = sign.sign();
+
+
+        String string = new String(signature);
+        System.out.println(string);
+        return string;
+
     }
 }
