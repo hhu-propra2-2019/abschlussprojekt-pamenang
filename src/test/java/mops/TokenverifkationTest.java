8efea2df46cc4658efb963711ddb8799ce510f1c
Zumoari <joshua.kokol@web.de>
2020-03-05 11:37:28 +0100

    Update TokenverifikationTest

--- src/test/java/mops/TokenverifkationTest.java
+++ src/test/java/mops/TokenverifkationTest.java
@@ -3,17 +3,21 @@ package mops;
 import mops.klausurzulassung.Token.Tokenverifikation;
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class TokenverifkationTest {
 
     @Test
-    public void testverifikation(){
+    public void testverifikation() throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
         String token="de8944d29e5f01ad1f6f9f0203bcbc11e2809c72706b43d4bbdc77d7d4c32288";
         String matr ="2770736";
         String fachid="1111111";
 
-        assertTrue(Tokenverifikation.verifikation(matr,fachid,token, pk));
+        assertTrue(Tokenverifikation.verifikationToken(matr,fachid,token));
     }
 }
