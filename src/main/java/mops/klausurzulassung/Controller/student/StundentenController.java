dcb603352c937025690ef34532adcf60ebcb890f
Fabian <f-mersch@web.de>
2020-03-05 10:50:28 +0100

    Request Route geändert, Mops style Update

--- src/main/java/mops/klausurzulassung/Controller/student/StundentenController.java
+++ src/main/java/mops/klausurzulassung/Controller/student/StundentenController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/zulassung1")
 @Controller
 public class StundentenController {
 
