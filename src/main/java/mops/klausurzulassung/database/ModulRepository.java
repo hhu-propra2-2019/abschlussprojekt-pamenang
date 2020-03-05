e4ab5669bbd483eedc1a9ce8b8b5aece9559fa75
caueb <caueb100@hhu.de>
2020-03-05 08:37:40 +0100

    Modul Auwahl

--- src/main/java/mops/klausurzulassung/database/ModulRepository.java
+++
@@ -1,7 +0,0 @@
-package mops.klausurzulassung.database;
-
-import mops.klausurzulassung.model.Modul;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ModulRepository extends JpaRepository<Modul, String> {
-}
