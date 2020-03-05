2d3bc4e1fccce57bf63858c2ae0348e142e2c402
floradzi <florian.radziej@gmail.com>
2020-03-05 10:29:11 +0100

    Update der Tokenverifikation zur neuen Verschlüsselungsmethode

--- src/main/java/mops/klausurzulassung/Token/Tokenverifikation.java
+++ src/main/java/mops/klausurzulassung/Token/Tokenverifikation.java
@@ -1,10 +1,34 @@
 package mops.klausurzulassung.Token;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
+import java.security.SignatureException;
+
 public class Tokenverifikation {
 
     //WICHTIG!!!
     //Key muss noch angepasst werden!
-    public static boolean verifikation(String matr, String fachID, String token){
-        return null;
+    public static boolean verifikation(String matr, String fachID, String token) throws NoSuchAlgorithmException, SignatureException {
+
+        Signature sign = Signature.getInstance("SHA256withRSA");
+
+
+        byte[] bytes = hexStringToByteArray(token);
+
+        sign.initVerify(/*get Public key*/);
+        sign.update(bytes);
+
+        return sign.verify(bytes);
+
+    }
+
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
     }
 }
