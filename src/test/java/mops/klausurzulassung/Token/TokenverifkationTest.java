e7697f78abd23009770d7541528e6f438fc95734
Zumoari <joshua.kokol@web.de>
2020-03-05 12:09:35 +0100

    Löschen überflüssiger imports in TokenverifikationTest

--- src/test/java/mops/klausurzulassung/Token/TokenverifkationTest.java
+++ src/test/java/mops/klausurzulassung/Token/TokenverifkationTest.java
@@ -1,9 +1,6 @@
-package mops;
+package mops.klausurzulassung.Token;
 
-import mops.klausurzulassung.Token.Tokenverifikation;
 import org.junit.jupiter.api.Test;
-
-import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
 
@@ -13,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TokenverifkationTest {
 
     @Test
-    public void testverifikation() throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+    public void testverifikation() throws NoSuchAlgorithmException, SignatureException{
         String token="de8944d29e5f01ad1f6f9f0203bcbc11e2809c72706b43d4bbdc77d7d4c32288";
         String matr ="2770736";
         String fachid="1111111";
