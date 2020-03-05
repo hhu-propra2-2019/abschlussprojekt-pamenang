a8abb86883193dc4b7b8881a6ef05685726589dc
floradzi <florian.radziej@gmail.com>
2020-03-05 08:46:14 +0100

    Test der Methode verifikation

--- src/test/java/mops/TokenverifkationTest.java
+++ src/test/java/mops/TokenverifkationTest.java
@@ -1,4 +1,19 @@
 package mops;
 
+import mops.klausurzulassung.Token.Tokenverifikation;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 public class TokenverifkationTest {
+
+    @Test
+    public void testverifikation(){
+        String token="de8944d29e5f01ad1f6f9f0203bcbc11e2809c72706b43d4bbdc77d7d4c32288";
+        String matr ="2770736";
+        String fachid="1111111";
+
+        assertTrue(Tokenverifikation.verifikation(matr,fachid,token));
+    }
 }
