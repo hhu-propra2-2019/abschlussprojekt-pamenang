d99b1a0e99d0017d663d90d4571c2f00afa98357
Fabian <f-mersch@web.de>
2020-03-05 09:56:43 +0100

    Studententest hinzugefügt

--- src/test/java/mops/Studententest.java
+++ src/test/java/mops/Studententest.java
@@ -1,14 +1,14 @@
 package mops;
 
-
 import mops.klausurzulassung.Controller.student.StundentenController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -18,16 +18,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class Studententest {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @MockBean StundentenController student;
+  @Autowired private MockMvc mockMvc;
+  @Autowired private WebApplicationContext context;
 
-    @MockBean
-    StundentenController student;
+  @Test
+  public void fuerAltzulassungAnmelden() throws Exception {
+    mockMvc.perform(get("/student")).andExpect(status().is3xxRedirection()).andDo(print());
+  }
 
-
-    @Test
-    public void fuerAltzulassungAnmelden() throws Exception {
-        String expect = "Hi";
-        mockMvc.perform(get("/student")).andExpect(status().is3xxRedirection()).andDo(print());
-    }
+  @WithMockUser(username = "niemand", password = "nichts")
+  @Test
+  public void anmeldung() throws Exception {
+    mockMvc.perform(get("/student")).andExpect(status().isForbidden());
+  }
 }
