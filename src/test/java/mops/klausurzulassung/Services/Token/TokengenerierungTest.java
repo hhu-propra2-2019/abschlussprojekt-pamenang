432d526fe70b5f8a9e47632c421df006c877ed5f
Zumoari <joshua.kokol@web.de>
2020-03-06 11:00:24 +0100

    Vervollständigen des Tokengenerierung Test + Teile in TokenverifikationTest

--- src/test/java/mops/klausurzulassung/Services/Token/TokengenerierungTest.java
+++ src/test/java/mops/klausurzulassung/Services/Token/TokengenerierungTest.java
@@ -1,8 +1,18 @@
 package mops.klausurzulassung.Services.Token;
 
+import mops.klausurzulassung.Services.Token.Repositories.QuittungRepository;
+import mops.klausurzulassung.Services.Token.Services.QuittungService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class TokengenerierungTest {
 
@@ -12,8 +22,9 @@ public class TokengenerierungTest {
   public void testErstellenHashValue() {
     String matr = "3333333";
     String fach = "propra1";
+    QuittungService quittungService = mock(QuittungService.class);
 
-    TokengenerierungService tg = new TokengenerierungService();
+    TokengenerierungService tg = new TokengenerierungService(quittungService);
     String ergebnis = tg.erstellenHashValue(matr, fach);
 
     assertEquals(ergebnis, "3333333propra1");
@@ -24,8 +35,9 @@ public class TokengenerierungTest {
     String matr = "3333333";
     String fach = "propra1";
     String token = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3";
+    QuittungService quittungService = mock(QuittungService.class);
 
-    TokengenerierungService tg = new TokengenerierungService();
+    TokengenerierungService tg = new TokengenerierungService(quittungService);
     String ergebnis = tg.erstellenQuittung(matr, fach, token);
 
     assertEquals(
@@ -34,14 +46,28 @@ public class TokengenerierungTest {
 
   @Test
   public void testBytesToHex(){
+    QuittungService quittungService = mock(QuittungService.class);
 
     byte[] bytes ={10};
-    TokengenerierungService tg = new TokengenerierungService();
+    TokengenerierungService tg = new TokengenerierungService(quittungService);
     String test = tg.bytesToHex(bytes);
 
     assertEquals(test,"0A");
 
   }
 
+  @Test
+  public void testTokengenerierung() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+    QuittungRepository repository = mock(QuittungRepository.class);
+    QuittungService quittungService = new QuittungService(repository);
+    String matr = "1234567";
+    String fachID = "123";
+    TokengenerierungService tg = new TokengenerierungService(quittungService);
+
+    String token = tg.erstellenToken(matr, fachID);
+
+    assertThat(token).isNotNull();
+    verify(repository,times(1)).save(any());
+  }
 
 }
--- src/test/java/mops/klausurzulassung/Services/Token/TokenverifkationTest.java
+++ src/test/java/mops/klausurzulassung/Services/Token/TokenverifkationTest.java
@@ -1,19 +1,36 @@
 package mops.klausurzulassung.Services.Token;
 
+import mops.klausurzulassung.Services.Token.Repositories.QuittungRepository;
+import mops.klausurzulassung.Services.Token.Services.QuittungService;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
 
 public class TokenverifkationTest {
 
     @Test
     public void testHexStringToByteArray(){
+        QuittungService quittungService = mock(QuittungService.class);
         String hexString = "0A";
         byte b = 10;
-        TokenverifikationService tv = new TokenverifikationService();
+        TokenverifikationService tv = new TokenverifikationService(quittungService);
         byte[] bytes = tv.hexStringToByteArray(hexString);
 
         assertEquals(bytes[0], b);
+    }
+
+    @Test
+    public void testTokenverifikation(){
+        QuittungRepository repository = mock(QuittungRepository.class);
+        QuittungService quittungService = new QuittungService(repository);
+
+        TokenverifikationService tv = new TokenverifikationService(quittungService);
+
+        tv.verifikationToken();
+
+        assertTrue(true);
     }
 }
