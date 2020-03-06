db9fc9657300cefee2512a00a2f5e61ba647c4d7
Zumoari <joshua.kokol@web.de>
2020-03-06 09:00:25 +0100

    Füllen der Entity Quittung

--- src/main/java/mops/klausurzulassung/Services/Token/Entities/Quittung.java
+++ src/main/java/mops/klausurzulassung/Services/Token/Entities/Quittung.java
@@ -1,4 +1,25 @@
 package mops.klausurzulassung.Services.Token.Entities;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "quittungen")
 public class Quittung {
+
+    @Column(name = "Matrikelnummer")
+    private String matrikelnummer;
+
+    @Column(name = "FachID")
+    private String fachID;
+
+    @Column(name = "PublicKey")
+    private String publicKey;
+
+    @Column(name = "Token")
+    private String token;
 }
