8abb48bcc7d0fb544bd90a4d76d5e3585e8424c4
Hendrik Schmitt <hendrik.schmitt.mail@web.de>
2020-03-10 09:19:48 +0100

    Refactoring des ModulControllers aufgrund von SpotBugs Fehler, dabei beheben eines Fehler, weshalb der Student nicht gespeichert werden konnte

--- src/main/java/mops/klausurzulassung/Domain/Student.java
+++ src/main/java/mops/klausurzulassung/Domain/Student.java
@@ -2,23 +2,23 @@ package mops.klausurzulassung.Domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "students")
 public class Student {
 
   private String vorname;
   private String nachname;
   private String email;
-  @Id
-  private Long matrikelnummer;
+  @Id private Long matrikelnummer;
   private Long modulId;
   private String fachname;
-  private String token;
-
+  @Lob private String token;
 }
-
--- src/main/java/mops/klausurzulassung/Services/CsvService.java
+++ src/main/java/mops/klausurzulassung/Services/CsvService.java
@@ -1,6 +1,5 @@
 package mops.klausurzulassung.Services;
 
-
 import com.opencsv.CSVWriter;
 import mops.klausurzulassung.Domain.Student;
 import org.apache.commons.csv.CSVFormat;
@@ -26,7 +25,7 @@ public class CsvService {
   }
 
   /*CsvImportService kümmert sich um ein Multipartfile welches ein .csv File repräsentiert. Aus diesem File werden Studenten-Objekte
-    generiert die als Liste weitergegeben werden*/
+  generiert die als Liste weitergegeben werden*/
 
   // Reihenfolge im input.csv:
   // Vorname, Nachname, Email, Matrikelnummer
@@ -34,10 +33,13 @@ public class CsvService {
   // Reihenfolge im output.csv:
   // Matrikelnummer, Nachname, Vorname
 
-  public List<Student> getStudentListFromInputFile(MultipartFile multipartFile, Long id) throws IOException {
+  public List<Student> getStudentListFromInputFile(MultipartFile multipartFile, Long id)
+      throws IOException {
     List<Student> studentList = new ArrayList<>();
 
-    Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(new InputStreamReader(multipartFile.getInputStream(), StandardCharsets.UTF_8));
+    Iterable<CSVRecord> records =
+        CSVFormat.DEFAULT.parse(
+            new InputStreamReader(multipartFile.getInputStream(), StandardCharsets.UTF_8));
 
     for (CSVRecord record : records) {
       studentList.add(createStudentFromInputStream(record, id));
@@ -46,7 +48,9 @@ public class CsvService {
   }
 
   public void putStudentOntoList(CSVWriter writer, Student student) {
-    String[] list = {String.valueOf(student.getMatrikelnummer()), student.getNachname(), student.getVorname()};
+    String[] list = {
+      String.valueOf(student.getMatrikelnummer()), student.getNachname(), student.getVorname()
+    };
 
     writer.writeNext(list, false);
   }
@@ -68,8 +72,8 @@ public class CsvService {
   }
 
   public void writeCsvFile(Long id, List<Student> students) throws IOException {
-    File outputFile = new File("klausurliste_"+id+".csv");
-    FileWriter fileWriter = new FileWriter(outputFile, StandardCharsets.UTF_8);
+    File outputFile = new File("klausurliste_" + id + ".csv");
+    FileWriter fileWriter = new FileWriter(outputFile);
     CSVWriter writer = new CSVWriter(fileWriter);
 
     Iterable<Student> altzugelassene = studentService.findByModulId(id);
