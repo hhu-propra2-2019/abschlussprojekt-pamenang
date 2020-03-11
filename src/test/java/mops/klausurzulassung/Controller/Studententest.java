108f1f771c6ac97cce7b0889f52d4449f439dda3
Fabian <f-mersch@web.de>
2020-03-11 12:03:30 +0100

    Alten Import gelöscht

--- src/test/java/mops/klausurzulassung/Controller/Studententest.java
+++ src/test/java/mops/klausurzulassung/Controller/Studententest.java
@@ -1,6 +1,5 @@
 package mops.klausurzulassung.Controller;
 
-import mops.klausurzulassung.Controller.StudentenController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
