ce580ade2f97bd9b4f9d72e6eb55846564648d73
tohof103 <tohof103>
2020-03-11 12:19:54 +0100

    Einrcuekung angepasst

--- src/test/java/mops/klausurzulassung/Services/ModulServiceTest.java
+++ src/test/java/mops/klausurzulassung/Services/ModulServiceTest.java
@@ -2,7 +2,6 @@ package mops.klausurzulassung.Services;
 
 import mops.klausurzulassung.Domain.Student;
 import mops.klausurzulassung.Repositories.ModulRepository;
-import org.bouncycastle.math.raw.Mod;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,44 +13,58 @@ import static org.mockito.Mockito.mock;
 
 public class ModulServiceTest {
 
-    private ModulRepository modulRepository;
-    private CsvService csvService;
-    private StudentService studentService;
-    private TokengenerierungService tokengenerierungService;
-    private EmailService emailService;
-    private QuittungService quittungService;
+  private ModulRepository modulRepository;
+  private CsvService csvService;
+  private StudentService studentService;
+  private TokengenerierungService tokengenerierungService;
+  private EmailService emailService;
+  private QuittungService quittungService;
 
-    @BeforeEach
-    public void initilize(){
-        modulRepository = mock(ModulRepository.class);
-        csvService = mock(CsvService.class);
-        studentService = mock(StudentService.class);
-        tokengenerierungService = mock(TokengenerierungService.class);
-        emailService = mock(EmailService.class);
-        quittungService = mock(QuittungService.class);
-    }
+  @BeforeEach
+  public void initilize() {
+    modulRepository = mock(ModulRepository.class);
+    csvService = mock(CsvService.class);
+    studentService = mock(StudentService.class);
+    tokengenerierungService = mock(TokengenerierungService.class);
+    emailService = mock(EmailService.class);
+    quittungService = mock(QuittungService.class);
+  }
 
+  @Test
+  void testStudentIsEmptyIsEmpty() {
+    Student student = new Student();
+    student.setVorname(anyString());
+    student.setNachname(anyString());
+    student.setEmail(null);
+    student.setMatrikelnummer(anyLong());
+    ModulService modulService =
+        new ModulService(
+            modulRepository,
+            csvService,
+            studentService,
+            tokengenerierungService,
+            emailService,
+            quittungService);
+    boolean b = modulService.studentIsEmpty(student);
+    assertTrue(b);
+  }
 
-    @Test
-    void testStudentIsEmptyIsEmpty(){
-        Student student = new Student();
-        student.setVorname(anyString());
-        student.setNachname(anyString());
-        student.setEmail(null);
-        student.setMatrikelnummer(anyLong());
-        ModulService modulService = new ModulService(modulRepository,csvService,studentService, tokengenerierungService,emailService,quittungService);
-        boolean b = modulService.studentIsEmpty(student);
-        assertTrue(b);
-    }
-    @Test
-    void testStudentIsEmptyIsNotEmpty(){
-        ModulService modulService = new ModulService(modulRepository,csvService,studentService, tokengenerierungService,emailService,quittungService);
-        Student student = new Student();
-        student.setVorname(anyString());
-        student.setNachname(anyString());
-        student.setEmail(anyString());
-        student.setMatrikelnummer(anyLong());
-        boolean b = modulService.studentIsEmpty(student);
-        assertFalse(b);
-    }
+  @Test
+  void testStudentIsEmptyIsNotEmpty() {
+    ModulService modulService =
+        new ModulService(
+            modulRepository,
+            csvService,
+            studentService,
+            tokengenerierungService,
+            emailService,
+            quittungService);
+    Student student = new Student();
+    student.setVorname(anyString());
+    student.setNachname(anyString());
+    student.setEmail(anyString());
+    student.setMatrikelnummer(anyLong());
+    boolean b = modulService.studentIsEmpty(student);
+    assertFalse(b);
+  }
 }
