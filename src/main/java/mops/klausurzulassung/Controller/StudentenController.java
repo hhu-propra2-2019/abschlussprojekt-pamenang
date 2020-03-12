419162d9c9c4eda8e5e41d6dc5aacbb2b8db55c1
Hendrik Schmitt <hendrik.schmitt.mail@web.de>
2020-03-12 09:41:35 +0100

    Fehlermeldung hinzufügen

--- src/main/java/mops/klausurzulassung/Controller/StudentenController.java
+++ src/main/java/mops/klausurzulassung/Controller/StudentenController.java
@@ -12,12 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.annotation.SessionScope;
 
+import javax.validation.Valid;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
@@ -41,13 +43,13 @@ public class StudentenController {
   }
 
   @Secured({"ROLE_studentin", "ROLE_orga"})
-  @GetMapping("/student/{zulassungToken}/{fachID}/{matrikelnr}/{vorname}/{nachname}/")
-  public String studentansichtMitToken(@PathVariable String zulassungToken, @PathVariable String fachID, @PathVariable long matrikelnr, @PathVariable String vorname, @PathVariable String nachname, Model model, KeycloakAuthenticationToken token) {
-    model.addAttribute("account", createAccountFromPrincipal(token));
+  @GetMapping("/student/{token}/{fach}/{matrikelnummer}/{vorname}/{nachname}/")
+  public String studentansichtMitToken(@PathVariable String token, @PathVariable String fach, @PathVariable long matrikelnummer, @PathVariable String vorname, @PathVariable String nachname, Model model, KeycloakAuthenticationToken keyToken) {
+    model.addAttribute("account", createAccountFromPrincipal(keyToken));
     model.addAttribute("meldung", false);
-    model.addAttribute("zulassungToken", zulassungToken);
-    model.addAttribute("matrikelnr", matrikelnr);
-    model.addAttribute("fach", fachID);
+    model.addAttribute("token", token);
+    model.addAttribute("matrikelnummer", matrikelnummer);
+    model.addAttribute("fach", fach);
     model.addAttribute("vorname", vorname);
     model.addAttribute("nachname", nachname);
     return "student";
@@ -59,6 +61,11 @@ public class StudentenController {
     model.addAttribute("account", createAccountFromPrincipal(token));
     model.addAttribute("meldung", false);
     model.addAttribute("student", false);
+    model.addAttribute("token", "");
+    model.addAttribute("matrikelnummer", "");
+    model.addAttribute("fach", "");
+    model.addAttribute("vorname", "");
+    model.addAttribute("nachname", "");
     if (token.getAccount().getPrincipal().toString().equals("studentin")){
       model.addAttribute("student", true);
       System.out.println(token.getAccount().getPrincipal().toString());
@@ -69,7 +76,7 @@ public class StudentenController {
 
   @PostMapping("/student")
   @Secured({"ROLE_studentin", "ROLE_orga"})
-  public String empfangeDaten(KeycloakAuthenticationToken keycloakAuthenticationToken, Model model, String matrikelnummer, String token, String fach, String vorname, String nachname, String email)
+  public String empfangeDaten(KeycloakAuthenticationToken keycloakAuthenticationToken, Model model, @Valid String matrikelnummer, @Valid String token, @Valid String fach, @Valid String vorname, @Valid String nachname, String email)
       throws SignatureException, NoSuchAlgorithmException, InvalidKeyException,
           NoPublicKeyInDatabaseException {
 
