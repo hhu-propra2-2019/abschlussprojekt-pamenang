b171614abf274a8e33ac1fcceb08a6537d4d7b5c
R4R3H3N1 <hendrik.schmitt.mail@web.de>
2020-03-18 11:23:54 +0100

    Unnötige Imports gelöscht

--- src/test/java/mops/klausurzulassung/Controller/ModulAuswahlTests.java
+++ src/test/java/mops/klausurzulassung/Controller/ModulAuswahlTests.java
@@ -15,10 +15,6 @@ import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
