819b96b6b142f3513f404dfe50eb6ad6d3614cc2
Zumoari <joshua.kokol@web.de>
2020-03-23 09:02:38 +0100

    Bearbeiten des StatistikControllers
    Co-authored-by: R4R3H3N1 <hendrik.schmitt.mail@web.de>
    Co-authored-by: merschie <f-mersch@web.de>

--- src/main/java/mops/klausurzulassung/controller/StatistikController.java
+++ src/main/java/mops/klausurzulassung/controller/StatistikController.java
@@ -1,6 +1,14 @@
 package mops.klausurzulassung.controller;
 
+import mops.klausurzulassung.services.ModulService;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.annotation.SessionScope;
 
@@ -8,4 +16,13 @@ import org.springframework.web.context.annotation.SessionScope;
 @SessionScope
 @Controller
 public class StatistikController {
+
+  private Logger logger = LoggerFactory.getLogger(ModulService.class);
+
+  @Secured("ROLE_orga")
+  @GetMapping("/modul/{id}/statistik")
+  public String selectStatistik(@PathVariable Long id, Model model, KeycloakAuthenticationToken token) {
+
+    return "statistik";
+  }
 }
