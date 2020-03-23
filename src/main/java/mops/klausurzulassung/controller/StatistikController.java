343ea1db589895473aac94f2d622945bc3fa16eb
Zumoari <joshua.kokol@web.de>
2020-03-23 08:48:50 +0100

    Erstellen des StatistikControllers
    Co-authored-by: R4R3H3N1 <hendrik.schmitt.mail@web.de>
    Co-authored-by: merschie <f-mersch@web.de>

---
+++ src/main/java/mops/klausurzulassung/controller/StatistikController.java
@@ -0,0 +1,11 @@
+package mops.klausurzulassung.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.SessionScope;
+
+@RequestMapping("/zulassung1")
+@SessionScope
+@Controller
+public class StatistikController {
+}
