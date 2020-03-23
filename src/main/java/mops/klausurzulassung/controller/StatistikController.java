69f80a4fe781fe50cf568fcba785c387b4b1dab4
Fabian <f-mersch@web.de>
2020-03-23 10:00:19 +0100

    Modulname wird übergeben
    Co-authored-by: Joshua Kokol <joshua.kokol@web.de>
    Co-authored-by: R4R3H3N1 <hendrik.schmitt.mail@web.de>

--- src/main/java/mops/klausurzulassung/controller/StatistikController.java
+++ src/main/java/mops/klausurzulassung/controller/StatistikController.java
@@ -1,6 +1,9 @@
 package mops.klausurzulassung.controller;
 
+import mops.klausurzulassung.database_entity.Modul;
+import mops.klausurzulassung.domain.Account;
 import mops.klausurzulassung.services.ModulService;
+import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,17 +15,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.util.Optional;
+
 @RequestMapping("/zulassung1")
 @SessionScope
 @Controller
 public class StatistikController {
 
-  private Logger logger = LoggerFactory.getLogger(ModulService.class);
+  private final ModulService modulService;
+  private Logger logger = LoggerFactory.getLogger(StatistikController.class);
+
+  public StatistikController(ModulService modulService) {
+    this.modulService = modulService;
+  }
+
+  private Account createAccountFromPrincipal(KeycloakAuthenticationToken token) {
+    KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+    return new Account(principal.getName(),
+        principal.getKeycloakSecurityContext().getIdToken().getEmail(),
+        null,
+        token.getAccount().getRoles());
+  }
 
   @Secured("ROLE_orga")
   @GetMapping("/modul/{id}/statistik")
   public String selectStatistik(@PathVariable Long id, Model model, KeycloakAuthenticationToken token) {
-
+    Optional<Modul> modul = modulService.findById(id);
+    model.addAttribute("modul", modul.get());
     return "statistik";
   }
 }
