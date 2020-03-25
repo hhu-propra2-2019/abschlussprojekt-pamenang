190fca93a20198d225ac22ff5c814d4013496c8f
R4R3H3N1 <hendrik.schmitt.mail@web.de>
2020-03-25 09:46:16 +0100

    EmailController und EmailErrorPage template hinzugefügt und erste Implementierung geschrieben.
    Co-authored-by: floradzi <florian.radziej@gmail.com>
    Co-authored-by: merschie <f-mersch@web.de>
    Co-authored-by: Boroxx <tenelsen.boris@web.de>
    Co-authored-by: Zumoari <joshua.kokol@web.de>

---
+++ src/main/java/mops/klausurzulassung/controller/EmailErrorController.java
@@ -0,0 +1,58 @@
+package mops.klausurzulassung.controller;
+
+import mops.klausurzulassung.domain.Account;
+import mops.klausurzulassung.domain.EmailError;
+import mops.klausurzulassung.services.EmailErrorService;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+
+@Controller
+@SessionScope
+@RequestMapping("/zulassung1")
+public class EmailErrorController {
+
+  private EmailErrorService emailErrorService;
+
+ @Autowired
+  public EmailErrorController(EmailErrorService emailErrorService){
+    this.emailErrorService = emailErrorService;
+  }
+
+  private Account createAccountFromPrincipal(KeycloakAuthenticationToken token) {
+    KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+    return new Account(
+            principal.getName(),
+            principal.getKeycloakSecurityContext().getIdToken().getEmail(),
+            null,
+            token.getAccount().getRoles());
+  }
+
+  @Secured("ROLE_orga")
+  @GetMapping("/emailError/{modulId}")
+  public String emailErrorAnzeige(@PathVariable Long modulId, KeycloakAuthenticationToken token, Model model){
+    ArrayList<EmailError> filteredAfterModulIdEmailErrors = filterAllEmailErrors(emailErrorService.getEmailErrors(), modulId);
+    model.addAttribute("account", createAccountFromPrincipal(token));
+    model.addAttribute("emailErrors", filteredAfterModulIdEmailErrors);
+    return "/emailErrorPage";
+  }
+
+  private ArrayList<EmailError> filterAllEmailErrors(ArrayList<EmailError> allEmailErrors, Long modulId) {
+    ArrayList<EmailError> filteredList = new ArrayList<>();
+    for (EmailError emailError : allEmailErrors) {
+        if(emailError.getStudent().getModulId().equals(modulId)){
+          filteredList.add(emailError);
+        }
+    }
+    return filteredList;
+  }
+}
