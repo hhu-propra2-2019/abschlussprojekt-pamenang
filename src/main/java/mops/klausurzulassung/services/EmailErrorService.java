b1fdbc3487e56e4811be0f57a3f2172f139454c5
Boroxx <tenelsen.boris@web.de>
2020-03-26 11:59:04 +0100

    Javadoc für EmailErrorService geschrieben und build.gradle angepasst damit javadoc-files generiert werden
    Co-authored-by: Zumoari <joshua.kokol@web.de>
    Co-authored-by: R4R3H3N1 <hendrik.schmitt.mail@web.de>

--- src/main/java/mops/klausurzulassung/services/EmailErrorService.java
+++ src/main/java/mops/klausurzulassung/services/EmailErrorService.java
@@ -11,20 +11,39 @@ public class EmailErrorService {
 
     ArrayList<EmailError> emailErrors = new ArrayList<>();
 
+  /**
+   * This method adds an EmailErrorObject to the emailErrors-List
+   * @param emailError contains a StudentObject, when the Email was not send
+   */
     public void addEmailErrorToList(EmailError emailError){
       this.emailErrors.add(emailError);
     }
 
+
+  /**
+   * This method just returns the EmailErrorList
+   * @return emailError-List
+   */
     public ArrayList<EmailError> getEmailErrors(){
       return this.emailErrors;
     }
 
+  /**
+   * This method deletes EmailError form List based on a specific Student
+   * @param student contains the Student-Information
+   */
     public void deleteEmailErrorFromListWithStudent(Student student){
       EmailError emailError = findEmailErrorWithStudent(student);
       emailErrors.remove(emailError);
     }
 
-   EmailError findEmailErrorWithStudent(Student student) {
+  /**
+   * This method finds an EmailError by Student and returns this EmailError.
+   * If there is no Student in Email Error it returns a new StudentObject
+   * @param student contains the Student-Information
+   * @return EmailError-Object
+   */
+    EmailError findEmailErrorWithStudent(Student student) {
     for (EmailError emailError : emailErrors) {
         Student studentFromList = emailError.getStudent();
         if (studentFromList.equals(student)){
@@ -34,6 +53,13 @@ public class EmailErrorService {
     return new EmailError(new Student());
   }
 
+  /**
+   * This method finds a Student in EmailErrorList by Modul-ID and ID-Number.
+   * If there is no Student in Email Error it returns a new StudentObject
+   * @param modulId contains the Modul-ID
+   * @param matrikelNummer contains the ID-Number
+   * @return new Student-Object if there is no Student in EmailError
+   */
   public Student findStudentInListOfErrorEmails(Long modulId, Long matrikelNummer) {
     for (EmailError emailError : this.emailErrors) {
       Student student = emailError.getStudent();
